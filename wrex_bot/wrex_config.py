7a65cb902bede90cfdf0ac1d7ac86a91df28cb3a
LayfieldK <KevinjLayfield@gmail.Com>
2016-01-06 20:50:15 -0500

    Delete wrex_config.py

--- wrex_bot/wrex_config.py
+++
@@ -1,6 +0,0 @@
-REDDIT_USERNAME = 'WrexBot' #YOUR USERNAME as string
-REDDIT_PASS = 'password' # YOUR PASSWORD as string
-USER_AGENT = "raspberrypi:wrex_bt:v0.5 (by /u/evillego6)"
-CLIENTID = 'IxgFQnM30NEroA'
-CLIENTSECRET = '6UyA2DZA57NGvCEG1ACmI04llwU'
-WAIT = 45
