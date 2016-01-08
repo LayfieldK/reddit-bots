86f6298c969bf18b85dd0f3b59334fcd0c5ce620
layfieldk <kevinjlayfield@gmail.com>
2016-01-08 02:33:23 +0000

    Added logging. Added a banned user list. Added a post limit per hour to avoid bandwidth issues.  Added support for steam hardware, software, videos, and music. Did some work on the formatting of reply comments.

--- steampowered_bot/steampowered_bot.py
+++ steampowered_bot/steampowered_bot.py
@@ -8,15 +8,28 @@ import time
 import traceback
 import urllib
 import urllib2
+import logging
 from peewee import *
 from peewee import OperationalError
 from peewee import DoesNotExist
 from bs4 import BeautifulSoup
 from steampowered_config import *
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+logfile_name = "steampowered_bot_" + time.strftime("%m-%d-%Y") + ".log"
+handler = logging.FileHandler(logfile_name)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+logger.info("steampowered_bot started")
+
 # Verify that config file is present in current directory
 if not os.path.isfile("steampowered_config.py"):
-    print "Config file not found in current directory."
+    logger.error("Config file not found in current directory.")
     exit(1)
 
 db = SqliteDatabase('steampowered.db')
@@ -31,18 +44,18 @@ r.login(REDDIT_USERNAME, REDDIT_PASS)
 subreddit = r.get_subreddit('KevinBotTest')
 
 def initialize_db():
-    print "db opening"
+    logger.info("db connecting")
     db.connect()
     try:
-        db.create_tables([reply, comment_reply, submission_reply])
-        print "db opened"
+        db.create_tables([reply, comment_reply, submission_reply, banned_user])
+        logger.info("db opened")
     except OperationalError:
         # Table already exists. Do nothing
         pass
 
 def deinit():
     db.close()
-    print "db closed"
+    logger.info("db closed")
     
 
 class reply(Model):
@@ -65,11 +78,25 @@ def is_already_replied(comment_id):
     if comment_id in comments_replied_to:
         return True
     try:
-        comment_reply.select().where(
-            comment_reply.comment_id == comment_id).get()
+        comment_reply.select().where(comment_reply.comment_id == comment_id).get()
         return True
     except DoesNotExist:
         return False
+    
+def is_banned_user(username):
+    try:
+        banned_user.select().where(banned_user.username == username).get()
+        return True
+    except DoesNotExist:
+        return False
+    
+def has_reached_postlimit():
+    posts_within_hour = reply.select().where(reply.date_of_reply > datetime.datetime.utcnow() + datetime.timedelta(hours=-1)).count()
+    if posts_within_hour >= POSTLIMIT:
+        return True
+    else:
+        return False
+
     
 class submission_reply(Model):
     reply = ForeignKeyField(reply, related_name='submission_replies')
@@ -77,23 +104,22 @@ class submission_reply(Model):
     
     class Meta:
         database = db
-
-def get_date(comment):
-    time = comment.created
-    return datetime.datetime.fromtimestamp(time)
-
-def post_reply():
-    return
+        
+class banned_user(Model):
+    username = CharField()
+    
+    class Meta:
+        database = db
   
 def get_steamapp_ids(comment_body):
     # receives goodreads url
     # returns the id using regex
-    regex = "http://store\.steampowered\.com/app/(.*)/"
+    regex = "http://store\.steampowered\.com/app/(.*?)/"
     return set(re.findall(regex, comment_body))
 
 
 def get_steamapp_details(steamapp_id):
-    print steamapp_id
+    logger.info("getting details for steamapp %s", steamapp_id)
     
     steamapp_data = {}
     url = "http://store.steampowered.com/app/" + steamapp_id
@@ -104,47 +130,67 @@ def get_steamapp_details(steamapp_id):
     bsObj = BeautifulSoup( html.read())
     
     title_obj = bsObj.find("div", class_="apphub_AppName")
-    steamapp_data['title'] = title_obj.text.strip()
-    print title_obj.text.strip()
-    
+    steamapp_data['title'] = title_obj.get_text().strip()
+        
     game_desc_obj = bsObj.find("div", class_="game_description_snippet")
-    steamapp_data['game_desc'] = game_desc_obj.text.strip()
-    print game_desc_obj.text.strip()
+    steamapp_data['game_desc'] = game_desc_obj.get_text().strip()
     
     details_block = bsObj.find("div", class_="details_block")
 
     for br in details_block.findAll('br'):
         br.extract()
     
-    genrePattern = re.compile(r'Genre:')
-    steamapp_data['genre'] = details_block.find('b', text=genrePattern).find_next_sibling().text.strip()
-    print details_block.find('b', text=genrePattern).find_next_sibling().text.strip()
+    genre_pattern = re.compile(r'Genre:')
+    genre_label = details_block.find('b', text=genre_pattern)
+    if genre_label:
+        genre_objects = genre_label.find_next_siblings("a", href=re.compile("genre"))
+        genres = []     
+        for genre_object in genre_objects:
+            genres.append(genre_object.get_text().strip())
+        steamapp_data['genre'] = ", ".join(genres)
+        
+    dev_pattern = re.compile(r'Developer:')
+    dev_label = details_block.find('b', text=dev_pattern)
+    if dev_label:
+        steamapp_data['developer'] = dev_label.find_next_sibling().get_text().strip()
+        
+    pub_pattern = re.compile(r'Publisher:')
+    pub_label = details_block.find('b', text=pub_pattern)
+    if pub_label:
+        steamapp_data['publisher'] = pub_label.find_next_sibling().get_text().strip()
+        
+    release_date_pattern = re.compile(r'Release Date:')
+    release_date_label = details_block.find('b', text=release_date_pattern)
+    if release_date_label:
+        steamapp_data['release_date'] = release_date_label.next_sibling.strip()
     
-    devPattern = re.compile(r'Developer:')
-    steamapp_data['developer'] = details_block.find('b', text=devPattern).find_next_sibling().text.strip()
-    print details_block.find('b', text=devPattern).find_next_sibling().text.strip()
-    
-    pubPattern = re.compile(r'Publisher:')
-    steamapp_data['publisher'] = details_block.find('b', text=pubPattern).find_next_sibling().text.strip()
-    print details_block.find('b', text=pubPattern).find_next_sibling().text.strip()
-    
-    releaseDatePattern = re.compile(r'Release Date:')
-    steamapp_data['release_date'] = details_block.find('b', text=releaseDatePattern).next_sibling.strip()
-    
+    runtime_pattern = re.compile(r'Running Time:')
+    runtime_label = details_block.find('b', text=runtime_pattern)
+    if runtime_label:
+        steamapp_data['running_time'] = runtime_label.find_next_sibling().get_text().strip()
+        
+    production_pattern = re.compile(r'Production:')
+    production_label = details_block.find('b', text=production_pattern)
+    if production_label:
+        steamapp_data['production'] = production_label.find_next_sibling().get_text().strip()
+        
+    manufacturer_pattern = re.compile(r'Manufacturer:')
+    manufacturer_label = details_block.find('b', text=manufacturer_pattern)
+    if manufacturer_label:
+        steamapp_data['manufacturer'] = manufacturer_label.find_next_sibling().get_text().strip()
+        
     
     game_review_summary_obj = bsObj.find("span", class_="game_review_summary")
-    steamapp_data['game_review_summary'] = game_review_summary_obj.text.strip()
-    print game_review_summary_obj.text.strip()
+    steamapp_data['game_review_summary'] = game_review_summary_obj.get_text().strip()
     
     glance_ctn = bsObj.find("div",class_="glance_ctn_responsive_left")
     steamapp_data['game_review_stats'] = glance_ctn.div["data-store-tooltip"].strip()
-    print glance_ctn.div["data-store-tooltip"].strip()
     
     tag_objects = bsObj.find("div", class_="glance_tags popular_tags").find_all("a")
     tags = []
                     
     for tag_object in tag_objects:
-        tags.append(tag_object.text.strip())
+        tags.append(tag_object.get_text().strip())
     
     steamapp_data['tags'] = ", ".join(tags)
         
@@ -153,11 +199,11 @@ def get_steamapp_details(steamapp_id):
     steamapp_data['original_price'] = ""
     steamapp_data['discount_percentage'] = ""
     if price_section.find("div", class_="game_purchase_price"):
-        steamapp_data['current_price'] = price_section.find("div", class_="game_purchase_price").text.strip()
+        steamapp_data['current_price'] = price_section.find("div", class_="game_purchase_price").get_text().strip()
     elif price_section.find("div", class_="discount_original_price"):
-        steamapp_data['current_price'] = price_section.find("div", class_="discount_final_price").text.strip()
-        steamapp_data['original_price'] = price_section.find("div", class_="discount_original_price").text.strip()
-        steamapp_data['discount_percentage'] = price_section.find("div", class_="discount_pct").text.strip().replace("-","")
+        steamapp_data['current_price'] = price_section.find("div", class_="discount_final_price").get_text().strip()
+        steamapp_data['original_price'] = price_section.find("div", class_="discount_original_price").get_text().strip()
+        steamapp_data['discount_percentage'] = price_section.find("div", class_="discount_pct").get_text().strip().replace("-","")
     else :
         steamapp_data['current_price'] = "N/A"
     return steamapp_data
@@ -165,33 +211,48 @@ def get_steamapp_details(steamapp_id):
 def add_steamapp_details_to_reply(reply_text,steamapp_data):
     reply_text +=  "|||\n"
     reply_text += "|:--|:--|\n"
-    reply_text += "|Name|" + steamapp_data['title'] + "|\n"
-    reply_text += "|Description|" + steamapp_data['game_desc'] + "|\n"
-    reply_text += "|Price|" 
+    reply_text += "|**Name**|" + steamapp_data['title'] + "|\n"
+    if "game_desc" in steamapp_data:
+        reply_text += "|**Description**|" + steamapp_data['game_desc'] + "|\n"
+    if "genre" in steamapp_data:
+        reply_text += "|**Genre**|" + steamapp_data['genre'] + "|\n"
+    if "running_time" in steamapp_data:
+        reply_text += "|**Running Time**|" + steamapp_data['running_time'] + "|\n"
+    reply_text += "|**Price**|" 
     
     if steamapp_data['discount_percentage'] == "" :
         reply_text += steamapp_data['current_price'] + "|\n"
     else :
-        reply_text += steamapp_data['original_price'] + "  -  " + steamapp_data['discount_percentage'] + "  =  " + steamapp_data['current_price'] + "|\n"
-        
-    reply_text += "|Steam Reviews|" + steamapp_data['game_review_summary'] + " - " + steamapp_data['game_review_stats'] + "|\n"
-    reply_text += "|Popular Tags|" + steamapp_data['tags'] + "|\n"
-    reply_text += "|Developer|" + steamapp_data['developer'] + "|\n"
-    reply_text += "|Publisher|" + steamapp_data['publisher'] + "|\n"
-    reply_text += "|Release Date|" + steamapp_data['release_date'] + "|\n"
-    reply_text += "\n\n"
+        reply_text += "~~" + steamapp_data['original_price'] + "~~  -  " + steamapp_data['discount_percentage'] + "  =  **" + steamapp_data['current_price'] + "**|\n"
+    
+    if "game_review_summary" in steamapp_data:    
+        reply_text += "|**Steam Reviews**|" + steamapp_data['game_review_summary'] + " - " + steamapp_data['game_review_stats'] + "|\n"
+    if "tags" in steamapp_data:
+        reply_text += "|**Popular Tags**|" + steamapp_data['tags'] + "|\n"
+    if "developer" in steamapp_data:
+        reply_text += "|**Developer**|" + steamapp_data['developer'] + "|\n"
+    if "publisher" in steamapp_data:
+        reply_text += "|**Publisher**|" + steamapp_data['publisher'] + "|\n"
+    if "manufacturer" in steamapp_data:
+        reply_text += "|**Manufacturer**|" + steamapp_data['manufacturer'] + "|\n"
+    if "production" in steamapp_data:
+        reply_text += "|**Production**|" + steamapp_data['production'] + "|\n"
+    if "release_date" in steamapp_data:
+        reply_text += "|**Release Date**|" + steamapp_data['release_date'] + "|\n"
+    reply_text += "\n&nbsp;\n***\n\n"
     return reply_text
 
 def process_reply_to_comment(comment):
-    print "Bot replying to : ", comment.id              
+    logger.info("Bot replying to : %s", comment.id)              
     steamapps = get_steamapp_ids(comment.body)
     reply_text = ""
     for steamapp in steamapps:
         steamapp_data = get_steamapp_details(steamapp)
         reply_text = add_steamapp_details_to_reply(reply_text,steamapp_data)
-        
+    
+    reply_text += "^^I ^^am ^^a ^^bot ^^created ^^for ^^fun. ^^Send ^^comments, ^^suggestions, ^^and ^^other ^^feedback ^^to ^^" + CREATOR_USER_PAGE + "."    
     post_reply_to_comment(comment,reply_text,steamapp)    
-    print "finished"
+    logger.info("comment %s replied to", comment.id)
     
 def post_reply_to_comment(comment,reply_text,steamapp_id):
     comment.reply(reply_text)
@@ -218,19 +279,23 @@ def main():
     try:
         # Gets all recent comments in subreddit
         for comment in subreddit.get_comments(limit=None):
-            print "checking comment " + comment.id
+            logger.info("checking comment %s", comment.id)
             # If this comment has not already been replied to by this bot
-              
-            if not is_already_replied(comment.id):
-                #if comment.author != None and comment.author.name != "steampowered_bot":
-                if comment.author != None:
-                    if "store.steampowered.com/app" in comment.body:
-                        process_reply_to_comment(comment)       
-
+            if not has_reached_postlimit():  
+                if not is_already_replied(comment.id):
+                    #if comment.author != None and comment.author.name != "steampowered_bot":
+                    if comment.author != None and not is_banned_user(comment.author):
+                        if "store.steampowered.com/app" in comment.body:
+                            process_reply_to_comment(comment)       
+                else:
+                    logger.info("comment %s already replied to", comment.id)
+            else:
+                logger.info("reached post limit, ignoring comments")
     except Exception as e:
         traceback.print_exc()
         print str(e)
         continueLoop = False
+        logger.error(str(e))
             
 initialize_db()
 main()
